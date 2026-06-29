dc0da1c9ed7020a018094c5595dd5fe983a07e4b
Ghrushnesh Rathod <55099077+ghrushneshr25@users.noreply.github.com>
2026-06-30 02:01:05 +0530

    Add hash map solution for Two Sum (Go)

---
+++ two-sum/hash-map.go
@@ -0,0 +1,15 @@
+func twoSum(nums []int, target int) []int {
+    indexMap := make(map[int]int)
+
+    for i, num := range nums {
+        complement := target - num
+
+        if idx, exists := indexMap[complement]; exists {
+            return []int{idx, i}
+        }
+
+        indexMap[num] = i
+    }
+
+    return []int{}
+}
